07213ca883c889d9366eb538bab2164810af608e
Vadim <lml@online.ua>
2020-10-08 12:34:55 -0400

    Doxygen files update

---
+++ docs/html/search/all_d.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['trackjunction_49',['TrackJunction',['../classTrackJunction.html',1,'']]],
+  ['trackjunction_2ecpp_50',['TrackJunction.cpp',['../TrackJunction_8cpp.html',1,'']]],
+  ['trackjunction_2eh_51',['TrackJunction.h',['../TrackJunction_8h.html',1,'']]],
+  ['trackpoint_52',['TrackPoint',['../classTrackPoint.html',1,'TrackPoint'],['../classTrackPoint.html#a2e567594e48ce98122283b2ffcfcca95',1,'TrackPoint::TrackPoint()']]],
+  ['trackpoint_2ecpp_53',['TrackPoint.cpp',['../TrackPoint_8cpp.html',1,'']]],
+  ['trackpoint_2eh_54',['TrackPoint.h',['../TrackPoint_8h.html',1,'']]],
+  ['train_55',['Train',['../classTrain.html',1,'']]],
+  ['train_2ecpp_56',['Train.cpp',['../Train_8cpp.html',1,'']]],
+  ['train_2eh_57',['Train.h',['../Train_8h.html',1,'']]]
+];
---
+++ docs/html/search/functions_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['trackpoint_120',['TrackPoint',['../classTrackPoint.html#a2e567594e48ce98122283b2ffcfcca95',1,'TrackPoint']]]
+];
